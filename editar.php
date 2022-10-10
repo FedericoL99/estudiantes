c75cd3981c7ec8ce3746c1bf3f80dc93e36d99c1
FedericoL <floaiza@unal.edu.co>
2022-10-10 12:21:50 -0500

    v4

--- editar.php
+++ editar.php
@@ -17,6 +17,7 @@ $row=mysqli_fetch_array($result);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EDITAR CON PHP</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 </head>
 <body>
 <nav class = "navbar navbar-dark bg-dark">
